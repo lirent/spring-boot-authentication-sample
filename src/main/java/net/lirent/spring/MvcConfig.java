50bb4ec9c10301a64a277ca56736036de5bed2ad
Lirent P <live@lirent.net>
2022-02-02 18:40:09 +0100

    comments

--- src/main/java/net/lirent/spring/MvcConfig.java
+++ src/main/java/net/lirent/spring/MvcConfig.java
@@ -7,6 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
+	/**
+	 * Assists with the registration of simple automated controllers
+	 * pre-configured with status code and/or a view.
+	 */
+
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/index").setViewName("index");
 		registry.addViewController("/").setViewName("index");
