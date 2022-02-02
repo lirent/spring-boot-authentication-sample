a233a9450b06bc2a0fea3e2c57a099c11d2c2817
Lirent P <live@lirent.net>
2022-02-02 18:24:16 +0100

    new values

--- src/main/java/net/lirent/spring/WebSecurityConfig.java
+++ src/main/java/net/lirent/spring/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package net.lirent.spring;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,17 +14,24 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	// !!password must be changed!!
+	@Value("${auth.security.username}")
+	private String username;
+	@Value("${auth.security.password}")
+	private String password;
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().passwordEncoder(passwordEncoder()).withUser("user")
-				.password(passwordEncoder().encode("password")).roles("USER");
+		auth.inMemoryAuthentication()
+		.passwordEncoder(passwordEncoder())
+		.withUser(username)
+		.password(passwordEncoder().encode(password))
+		.roles("USER");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/user").hasAnyRole("USER")
-		.antMatchers("/").permitAll()
+		.antMatchers("/")
+		.permitAll()
 		.and().formLogin();
 	}
 
